f139d267d250f4a38e6f414a4b5e4e4c1080d672
Wesley Moore <wes@wezm.net>
2023-06-05 20:01:03 +1000

    WIP

--- browser-extension/source/background.js
+++ browser-extension/source/background.js
@@ -1,5 +1,6 @@
 "use strict";
-const FEDIURL_HOST = new URL("http://127.0.0.1:8000/");
+// const FEDIURL_HOST = new URL("http://127.0.0.1:8000/");
+const FEDIURL_HOST = new URL("https://fediurl.7bit.org/");
 browser.browserAction.onClicked.addListener((tab) => {
     if (!tab.url) {
         return;
--- browser-extension/source/background.ts
+++ browser-extension/source/background.ts
@@ -1,6 +1,7 @@
 "use strict";
 
-const FEDIURL_HOST = new URL("http://127.0.0.1:8000/");
+// const FEDIURL_HOST = new URL("http://127.0.0.1:8000/");
+const FEDIURL_HOST = new URL("https://fediurl.7bit.org/");
 
 browser.browserAction.onClicked.addListener((tab) => {
 	if (!tab.url) {
